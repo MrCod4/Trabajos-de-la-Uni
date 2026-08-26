e960ff359944e313c0bfe758a58befb43fa7fcaf
MrCod4 <alonsossepulveda57@gmail.com>
2026-08-26 12:48:55 -0400

    ejercicios

--- C#/Clase 5/E1/Program.cs
+++ C#/Clase 5/E1/Program.cs
@@ -1,1 +1,66 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ejercicio1
+{
+    public class Vehiculo
+    {
+        public string Marca;
+        public string Modelo;
+        public string Patente;
+        public double Velocidad;
+
+        public void Acelerar()
+        {
+            Velocidad += 10;
+        }
+
+        public void Frenar()
+        {
+           Velocidad -= 10; 
+        }
+
+        public void Mostrar_info()
+        {
+            System.Console.WriteLine("-----Informaciion del vehiculo-----");
+            System.Console.WriteLine("Patente: "+Patente);
+            System.Console.WriteLine("Marca: "+Marca);
+            System.Console.WriteLine("Modelo: "+Modelo);
+            System.Console.WriteLine("Velocidad: "+Velocidad);
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            string patente;
+            string marca;
+            string modelo;
+            double velocidad;
+
+            System.Console.WriteLine("Ingrese la patente del auto");
+            patente = Console.ReadLine();
+
+            System.Console.WriteLine("Ingrese la marca del vehiculo");
+            marca = Console.ReadLine();
+
+            System.Console.WriteLine("Ingrese la modelo del vehiculo");
+            modelo = Console.ReadLine();
+
+            System.Console.WriteLine("Ingrese la velocidad del vehiculo");
+            velocidad = double.Parse(Console.ReadLine());
+
+            //Creacion del objeto 
+            Vehiculo v1 = new Vehiculo();
+            v1.Patente = patente;
+            v1.Marca = marca;
+            v1.Modelo = modelo;
+            v1.Velocidad = velocidad;
+
+            //uso de la funcion
+            v1.Acelerar();
+            v1.Mostrar_info();
+            
+        }
+    }
+}
---
+++ C#/Clase 5/E2/Program.cs
@@ -0,0 +1,83 @@
+﻿/*Complete el código C# para definir la clase Estudiante, agregue el método MostrarEstado() que evalúe si el
+alumno está aprobado (nota final ≥ 4.0) e instancie 2 objetos en el Main.
+*/
+
+using System;
+
+namespace ejercicio2
+{
+    class Estudiante
+    {
+
+        public string nombre;
+        public string carrera;
+        public double notaFinal;
+        
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Alumno: {nombre} | Carrera: {carrera} | Nota: {notaFinal}");
+        }
+
+        public void MostrarEstado()
+        {
+            if(notaFinal >= 4.0)
+            {
+                System.Console.WriteLine("Aprobo la materia");
+            }
+            else
+            {
+                System.Console.WriteLine("Reprobo el curso");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1;
+            string c1;
+            double nt1;
+
+            string n2;
+            string c2;
+            double nt2;
+
+            System.Console.WriteLine("Ingrese el nombre del estudiante 1");
+            n1 = Console.ReadLine();
+            
+            System.Console.WriteLine("Ingrese la carrera del estudiante 1");
+            c1 = Console.ReadLine();
+
+            System.Console.WriteLine("Ingrese la nota final del estudiante 1");
+            nt1 = double.Parse(Console.ReadLine());
+
+            Estudiante e1 = new Estudiante();
+            e1.nombre = n1;
+            e1.carrera = c1;
+            e1.notaFinal = nt1;
+
+            System.Console.WriteLine("Ingrese el nombre del estudiante 2");
+            n2 = Console.ReadLine();
+            
+            System.Console.WriteLine("Ingrese la carrera del estudiante 2");
+            c2 = Console.ReadLine();
+
+            System.Console.WriteLine("Ingrese la nota final del estudiante 2");
+            nt2 = double.Parse(Console.ReadLine());
+
+            Estudiante e2 = new Estudiante();
+            e2.nombre = n2;
+            e2.carrera = c2;
+            e2.notaFinal = nt2;
+
+            System.Console.WriteLine("--Estudiante 1--");
+            e1.MostrarDatos();
+            e1.MostrarEstado();
+
+            System.Console.WriteLine("--Estudiante 2--");
+            e2.MostrarDatos();
+            e2.MostrarEstado();
+
+        }
+    }
+}
