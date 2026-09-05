20d2e33861c086f3b3a1b92a766c26bf16714b8f
MrCod4 <alonsossepulveda57@gmail.com>
2026-09-04 21:31:19 -0400

    evaluacion 2

---
+++ C#/Ev2/E1/Program.cs
@@ -0,0 +1,85 @@
+﻿/*Sonic necesita probar el sistema de energía de sus escudos. Para ello, el laboratorio de Tails cuenta con una función moduladora de energía que analiza un valor numérico X en cada segundo y determina cuánta carga recibe el escudo y qué tipo de escudo debe desplegarse.
+Debes construir una solución modular en C# que implemente una función auxiliar utilizando paso de parámetros por referencia o salida ( out / ref ).
+
+
+Requerimientos de la Función:
+Crea un método llamado EvaluarEnergia que reciba el número de segundo actual X y retorne dos valores a través de parámetros out :
+
+Función carga :
+Si X es múltiplo de 3 y de 5 al mismo tiempo (múltiplo de 15), la carga obtenida es 100.
+Si X es múltiplo de 3 o de 5 (pero no de ambos a la vez), la carga obtenida es 50.
+Si X no es múltiplo ni de 3 ni de 5, la carga obtenida es 0.
+Función mensaje :
+Si la carga es 100: "[X]s (Carga 100%): ¡Súper Escudo Activado!"
+Si la carga es 50: "[X]s (Carga 50%): Escudo Normal Activado"
+Si la carga es 0: "[X]s (Carga 0%): Carga en proceso..."
+Programa Principal ( Main ):
+El programa debe solicitar/recibir un número entero N (1 ≤ N ≤ 100).
+Utilizando un bucle, debe llamar la función EvaluarEnergia para cada segundo desde 1 hasta N.
+Imprimir en pantalla el mensaje obtenido por el parámetro.
+
+En caso de ingresar un valor fuera del rango imprimir "Error, valor invalido" y finalizar el programa.
+
+Al momento de la ejecución no debe de existir el mensaje de nulabilidad.*/
+
+using System;
+namespace ejercicio1
+{
+    class Program
+    {
+        static void EvaluarEnergia(int segundo, out int carga)
+        {
+            if(segundo % 3 == 0 && segundo % 5 == 0)
+            {
+                carga = 100;
+            }
+            else if(segundo % 3 == 0 || segundo % 5 == 0)
+            {
+                carga = 50;
+            }
+            else
+            {
+                carga = 0;
+            }
+        }
+        static void EvaluarEnergia(ref int carga, int segundos)
+        {
+            if(carga == 100)
+            {
+                System.Console.WriteLine($"[{segundos}]s (Carga 100%): ¡Súper Escudo Activado!");
+            }
+            else if(carga == 50)
+            {
+                System.Console.WriteLine($"[{segundos}]s (Carga 50%): Escudo Normal Activado");
+            }
+            else
+            {
+                System.Console.WriteLine($"[{segundos}]s (Carga 0%): Carga en proceso...");
+            }
+        }
+        static void Main()
+        {
+            int segActual;
+            int carga;
+
+            System.Console.WriteLine("Ingrese un numero entre 1 y 100");
+            segActual = int.Parse(Console.ReadLine());
+
+            if(segActual < 100 && segActual > 1)
+            {
+                for(int i=1; i<=segActual; i++)
+                {
+                    EvaluarEnergia(i, out carga);
+                    EvaluarEnergia(ref carga, i);
+                    
+                    Thread.Sleep(1000);
+                    //sirve para hacer que haya un tiempo de espera entre siclos, a menos para este caso
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Error, valor invalido");
+            }
+        }
+    }
+}
---
+++ C#/Ev2/E2/Program.cs
@@ -0,0 +1,89 @@
+﻿/*Una reserva forestal cuenta con un sistema de monitoreo para la prevención y respuesta ante incendios forestales.
+Las condiciones ambientales pueden cambiar rápidamente y la central necesita clasificar las condiciones de una zona determinando su nivel de riesgo considerando tres mediciones ambientales: temperatura del aire, humedad relativa y velocidad del viento.
+La central ha establecido cuatro niveles: BAJO, MODERADO, ALTO y EXTREMO. El encargado de la estación solicita un programa en C# modular que analice las condiciones y reporte inmediatamente el nivel de riesgo.
+
+El programa solicitará tres valores enteros:
+Temperatura: Grados Celsius (°C), entre -30 y 60.
+Humedad relativa: Porcentaje (%), entre 0 y 100.
+Velocidad del viento: Kilómetros por hora (km/h), entre 0 y 250.
+Si algún dato de entrada está fuera de rango, el programa imprimirá únicamente: Error: datos de entrada no validos.
+
+Para garantizar un diseño modular, el cálculo debe implementarse en un método auxiliar llamado EvaluarRiesgo que reciba las mediciones e informe los resultados mediante parámetros out :
+
+static void EvaluarRiesgo(int temp, int humedad, int viento, out string nivel, out string
+alerta)
+Reglas de Clasificación (Orden Jerárquico):
+
+EXTREMO: Si la temperatura alcance al menos 35°C y, simultáneamente, la humedad sea como máximo 20%. Asigna nivel = "EXTREMO" y alerta = "ALERTA: Riesgo extremo detectado. Evacuacion activada." .
+ALTO: Si la velocidad del viento alcanza al menos 40 km/h y la humedad es como máximo 30%. Asigna nivel = "ALTO" y alerta = "" .
+MODERADO: Si la temperatura alcanza al menos 30°C o la humedad es como máximo 40%. Asigna nivel = "MODERADO" y alerta = "" .
+BAJO: Si ninguna de las anteriores se cumple. Asigna nivel = "BAJO" y alerta = "" .
+Si los datos son válidos, el programa imprimirá:
+Temp: T°C | Humedad: H% | Viento: V km/h -> Riesgo: NIVEL
+Si el nivel es EXTREMO, inmediatamente en la siguiente línea imprimirá el mensaje de alerta correspondiente.*/
+
+using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void EvaluarRiesgo(double celcius, double porcentaje, double velViento, out string nivel, out string alerta)
+        {
+            if(celcius >= 35 && porcentaje <= 20)
+            {
+                nivel = "EXTREMO";
+                alerta = "ALERTA: Riesgo extremo detectado. Evacuacion activada.";
+            }
+            else if(velViento >= 40 && porcentaje <= 30)
+            {
+                nivel = "ALTO";
+                alerta = "";
+            }
+            else if (celcius >= 30 && porcentaje <= 40)
+            {
+                nivel = "ALTO";
+                alerta = "";
+            }
+            else
+            {
+                nivel = "BAJO";
+                alerta = "";
+            }
+        }
+
+        static void Main()
+        {
+            double celcius;
+            double porcentaje;
+            double velViento;
+            string nivel;
+            string alerta;
+
+            System.Console.WriteLine("Ingrese la temperatura en celcius (entre -30 y 60)");
+            celcius = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Ingrese el porcentaje de humedad (entre 0 y 100)");
+            porcentaje = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Ingrese la velocidad del viento en km (entre 0 y 250)");
+            velViento = double.Parse(Console.ReadLine());
+
+            if (celcius >= -30 && celcius <= 60 && porcentaje <= 100 && porcentaje >= 0 && velViento <= 200 && velViento >= 0)
+            {
+                EvaluarRiesgo(celcius, porcentaje, velViento, out nivel, out alerta);
+
+                System.Console.WriteLine($"Temp: {celcius}°C | Humedad: {porcentaje}% | Viento: {velViento} km/h -> Riesgo: {nivel}");
+
+                if(nivel == "EXTREMO")
+                {
+                    System.Console.WriteLine($"{alerta}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Error: datos de entrada no validos.");
+            }
+        }
+    }
+}
